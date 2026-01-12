394a6e62d2d33983ad03289ceeb25ff3f1dc5c05
mauinGit <maulanaadiatma3133@gmail.com>
2026-01-12 19:53:33 +0700

    fix: debug bug user button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,17 @@
+'use client'
 import { Button } from "@/components/ui/button";
+import Modal from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="p-12">
-      <UserButton afterSignOutUrl="/sign-in"/>
+      <Button onClick={() => {}}>
+        <UserButton afterSignOutUrl="/sign-in" /> 
+      </Button>
+      <Modal title="Test-title" description="Test-description" isOpen={true} onClose={() => {}}>
+        Children
+      </Modal>
     </div>
   );
 }
